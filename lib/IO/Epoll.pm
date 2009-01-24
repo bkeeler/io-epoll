5d936ca1e176f9858984ee57674599ce0acbc47d
Bruce J Keeler <bruce@drangle.com>
2009-01-24 14:22:10 -0800

    Updated for version 0.02

--- lib/IO/Epoll.pm
+++ lib/IO/Epoll.pm
@@ -75,7 +75,7 @@ our @EXPORT = qw(
         epoll_pwait
 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
